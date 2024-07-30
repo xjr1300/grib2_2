d3e8110fd4a24e53bd126585bce5d7b24717ba7e
kuroyasu <xjr1300.04@gmail.com>
2024-07-30 10:40:49 +0900

    Configured a workspace.

--- examples/examples/prr_reader.rs
+++ examples/examples/prr_reader.rs
@@ -1,9 +1,9 @@
-use std::fs::OpenOptions;
-use std::io::{BufWriter, Write};
+use std::io::Write;
 
 use anyhow::anyhow;
 
 use grib2_2::readers::PrrReader;
+use helpers::{buf_writer, should_write_record};
 
 /// 解析雨量ファイル
 /// cspell: disable
@@ -15,19 +15,16 @@ const DST_PATH: &str = "resources/dst/Z__C_RJTD_20161121010000_SRF_GPV_Ggis1km_P
 
 fn main() -> anyhow::Result<()> {
     let mut reader = PrrReader::new(SRC_PATH)?;
-    let file = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .truncate(true)
-        .open(DST_PATH)?;
-    let iter = reader.record_iter().unwrap();
-    let mut writer = BufWriter::new(file);
-    writer.write_all(b"lon,lat,precipitation\n")?;
+    let iter = reader.record_iter()?;
+    let mut writer = buf_writer(DST_PATH)?;
+    writer.write_all(b"lon,lat,value\n")?;
     for record in iter.flatten() {
-        let lon = record.lon as f64 / 1e6;
-        let lat = record.lat as f64 / 1e6;
-        if let Some(value) = record.value {
-            writer.write_fmt(format_args!("{lon:.6},{lat:.6},{value}\n"))?;
+        if should_write_record(&record) {
+            let lon = record.lon as f64 / 1e6;
+            let lat = record.lat as f64 / 1e6;
+            if let Some(value) = record.value {
+                writer.write_fmt(format_args!("{lon:.6},{lat:.6},{value}\n"))?;
+            }
         }
     }
     writer.flush().map_err(|e| anyhow!(e))
